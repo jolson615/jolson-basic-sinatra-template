47b2f55eee4c62da1fbefeb43744e7640c841d42
Katie Farrell <kfarrell064@gmail.com>
2017-06-23 14:08:51 +0000

    updated the model

--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -28,7 +28,6 @@ class SnippetySnip
     
     
     def crustacean_altercation(enemy)
-    until self.health <= 0 || enemy.health <= 0
      if self.speed < enemy.speed
         self.health=(self.health - enemy.attack)
         enemy.health=(enemy.health - self.attack)
@@ -38,7 +37,6 @@ class SnippetySnip
          if enemy.health < 0
          enemy.health=(0)
          end
-        puts "#{enemy.name} attacked, you now have #{self.health} health...you attacked, #{enemy.name} now has #{enemy.health} health"
     elsif self.speed > enemy.speed
         enemy.health=(enemy.health - self.attack)
         self.health=(self.health - enemy.attack)
@@ -48,21 +46,9 @@ class SnippetySnip
          if enemy.health < 0
          enemy.health=(0)
          end
-        puts "you attacked, #{enemy.name} now has #{enemy.health} health...#{enemy.name} attacked, you now have #{self.health} health"
      end
-     end #until
     
-    if self.health == 0
-        puts "you lose! :("
-    elsif enemy.health == 0
-        puts "you win! :)"
-    end #if statement
+     return "After the attack, you're left with #{self.health} health and #{enemy.name} is left with #{enemy.health} health."
 
     end #method crustacean_altercation
 end #class 
-
-
-larry = SnippetySnip.new()
-barry = SnippetySnip.new("Barry")
-
-puts larry.crustacean_altercation(barry)
